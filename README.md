f9384f82073b578331302f3d89df39a51f7aeb03
Bora Lee <bbora1119@gmail.com>
2021-11-10 20:04:05 -0500

    delete done

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
 # Homework11_Express_NoteTaker
+
+
+
+## Description 
+
+You can create and save notes from the page. The saved notes will be displayed in the left side and if you click the left notes, then it presents to the right column. Also, you can go back to add-note mode by clicking + button at the top.
+
+
+
+## Table of Contents 
+
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [License](#License)
+- [Contributing](#Contributing)
+- [Test](#Test)
+- [Question](#Question)
+
+
+
+## Installation
+
+  ---
+  To install necessary depedencies, run the following command :
+  
+  **npm install**
+
+  ---
+
+## Usage
+
+  It uses Express and uuid modules.
+
+
+
+## License
+
+    There is no license here.
+    
+
+## Contributing
+
+nothing
+
+
+## Test
+
+  ---
+  To run the test, run the following command:
+  
+  ****
+
+  ---
+
+## Question
+
+If you have any question about the repo, open an issue or contact me directly at bbora1119@gmail.com. You can find more of my work at https://github.com/radoll93.
+
+
+
+
+
+  
--- routes/delete.js
+++ routes/delete.js
@@ -2,31 +2,31 @@ const deletedb = require('express').Router();
 const fs = require('fs');
 
 
-fs.readFile('./db/db.json', (err, data) => {
-        const parsingData = JSON.parse(data);
 
-        parsingData.forEach(noteobject => {
-            const {id} = noteobject;
+deletedb.delete('/:id', (req, res) => {
 
+    const { id } = req.params;
 
-
-deletedb.delete(`/${id}`, (req, res) => {
     
+    fs.readFile('./db/db.json', (err, data) => {
+    const parsingData = JSON.parse(data);
 
-    const idIndex = parsingData.indexOf(noteobject);
+    parsingData.forEach(noteobject => {
+        if(id === noteobject.id) {
+            const idIndex = parsingData.indexOf(noteobject);
 
-    parsingData.splice(idIndex, 1);
+            parsingData.splice(idIndex, 1);
 
-    fs.writeFile('./db/db.json', JSON.stringify(parsingData, null, 4), (err) => 
-              err ? console.error(err) : console.log('delete is done') )
 
-    res.json(parsingData);
 
+            fs.writeFile('./db/db.json', JSON.stringify(parsingData, null, 4), (err) => 
+                      err ? console.error(err) : console.log('delete is done') )
+        
+                      res.json(parsingData);
+
+        }
+    })
 })
-
-})
-
-
 })
 
 
